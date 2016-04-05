303333b18279c19de99ce7d3871bbb256fb7cd02
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-04-05 15:25:53 -0400

    startingmethod

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -5,4 +5,14 @@ public class Main {
     public static void main(String[] args) {
     	// write your code here
     }
+    static InventoryItems createItem(String name, int quantity, String category) {
+        //search for item by category
+        
+        //create Item for that category
+
+        //return item
+
+        //create exception if category is not found
+
+    }
 }
