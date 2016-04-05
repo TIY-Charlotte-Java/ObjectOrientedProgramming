8c0ac5c77aa18e6329b410ef7e00ca6bf41d107f
Ben Sterrett <raynjamin@gmail.com>
2016-04-05 14:00:44 -0400

    Adding basic project.

---
+++ test/com/theironyard/clt/MainTest.java
@@ -0,0 +1,16 @@
+package com.theironyard.clt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ben on 4/5/16.
+ */
+public class MainTest {
+
+    @Test
+    public void main() throws Exception {
+
+    }
+}
