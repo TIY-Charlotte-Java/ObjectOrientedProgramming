b3b142b7a3dd7bec5b92be85130e72641a3442ea
Ser-Castic <samsteinberg90@gmail.com>
2016-12-07 17:42:25 -0500

    Removed extra method, changed name of objects inside switch statements, working on system.out.print

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -5,36 +5,43 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Inventory item = new Inventory();
-        item.list.add(createItem("AppleWatch", 2, "Watch"));
-        item.list.add(createItem("Chocolate", 3, "IceCream"));
-        item.list.add(createItem("Hershey", 5, "CandyBar"));
-        item.list.add(createItem("iPhone7", 1, "Phone"));
-        item.list.add(createItem("Folgers", 4, "Coffee"));
-        item.list.add(createItem("Addidas", 40, "Shoes"));
+        ArrayList<InventoryItem> list = new ArrayList<>();
+        //Inventory item = new Inventory();
+        list.add(createItem("AppleWatch", 2, "Watch"));
+        list.add(createItem("Chocolate", 3, "IceCream"));
+        list.add(createItem("Hershey", 5, "CandyBar"));
+        list.add(createItem("iPhone7", 1, "Phone"));
+        list.add(createItem("Folgers", 4, "Coffee"));
+
+  //      System.out.print("\n" +);
+//        System.out.print("\n" +item.list.get(1));
+//        System.out.print("\n" +item.list.get(2));
+//        System.out.print("\n" +item.list.get(3));
+//        System.out.print("\n" +item.list.get(4));
+
     }
 
-    public static class Inventory {
-        ArrayList<InventoryItem> list = new ArrayList<>();
-    }
+//    public  class Inventory {
+//        ArrayList<InventoryItem> list = new ArrayList<>();
+//    }
 
     static InventoryItem createItem (String name, int quantity, String category) throws Exception {
         switch (category.toUpperCase()) {
             case "CANDYBAR":
                 System.out.printf("\nThere are %d %s bars in inventory", quantity, name);
-                return new CandyBar(name, quantity, category);
+                return new InventoryItem(name, quantity, category);
             case "WATCH":
                 System.out.printf("\nThere are %d %s watches in inventory", quantity, name);
-                return new Watch(name, quantity, category);
+                return new InventoryItem(name, quantity, category);
             case "PHONE":
                 System.out.printf("\nThere are %d %s phones in inventory", quantity, name);
-                return new Phone(name, quantity, category);
+                return new InventoryItem(name, quantity, category);
             case "ICECREAM":
                 System.out.printf("\nThere are %d %s ice cream flavors in inventory", quantity, name);
-                return new IceCream(name, quantity, category);
+                return new InventoryItem(name, quantity, category);
             case "COFFEE":
                 System.out.printf("\nThere are %d %s coffee brands in inventory", quantity, name);
-                return new Coffee(name, quantity, category);
+                return new InventoryItem(name, quantity, category);
             default:
                 throw new Exception("\nError: Not a category");
         }
