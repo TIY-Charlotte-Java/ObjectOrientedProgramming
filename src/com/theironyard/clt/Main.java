73b016b48606be4a14ebeeb88b43b218489d8786
Emileen Marianayagam <emileenmarianayagam@Emileens-MacBook-Pro.local>
2016-12-08 14:27:00 -0500

    attempt

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -10,49 +10,46 @@ public class Main {
         // write your code here
 
         ArrayList<InventoryItem> item = new ArrayList<>();
-       // InventoryItem product = new InventoryItem();
-        item.add(createItem("emi", 3, "book"));
+        item.add(createItem("emi", 1, "book"));
+        item.add(createItem("train", 1, "toy"));
+        item.add(createItem("megan", 1, "Dress"));
+        item.add(createItem("Skinny Pants", 1, "Pant"));
+        item.add(createItem("high heels", 1, "Shoes"));
 
 
     }
 
-    /*public static void Scanner(){
-        Scanner scanner = new Scanner(System.in);
-    }*/
-
-    public static InventoryItem createItem (String name, int quantity, String category) throws Exception{
+    public static InventoryItem createItem(String name, int quantity, String category) throws Exception {
         // create a static method -- createItem
         //returns an object using category class -- if you find the category then return that infromation which is in
         // there -- Book then return name,quantity and category else throws and error
-        //Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("please enter your category -- Book, Toy, Dress ,Shoes ,Pant,\n");
+        category = scanner.nextLine();
 
         switch (category) {
             case "Book":
-                System.out.printf("the category is %s, the quantity %d, the name is %s ", name , quantity,category);
-                return new Book(name,quantity,category);
-                //pritn book info
-                break;
+                System.out.printf("the category is %s, the quantity %d, the name is %s ", name, quantity, category);
+                return new Book(name, quantity, category);
+            //pritn book info
             case "Toy":
-                return new Toy(name,quantity,category);
-                //print toy info
-                break;
+                return new Toy(name, quantity, category);
+            //print toy info
             case "Dress":
-                //print dress info
-                break;
+                return new Dress(name, quantity, category);
+            //print dress info
             case "Shoes":
-                //print shoes info
-                break;
+                return new Shoes(name, quantity, category);
+            //print shoes info
             case "Pant":
-                //print pant info
-                break;
-
+                return new Pant(name, quantity, category);
+            //print pant info
             default:
                 System.out.println("you have entered an invalid category");
                 throw new Exception("incorrect information");
                 //  System.out.println(createItem("book", 2, "book"));
         }
 
-        //return createItem("flower",1,"toy");
-        //return createItem("emi", 1, "book");
-    return;
+    }
 }
